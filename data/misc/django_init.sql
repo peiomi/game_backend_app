277e49595c6e911f1e9ddc4c1067797f97620c74
paydon <paydonriley@gmail.com>
2026-01-26 15:34:34 -0600

    docker compose image

--- data/misc/django_init.sql
+++ data/misc/django_init.sql
@@ -6,7 +6,7 @@ CREATE DATABASE game_db;
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard conforming_strings = on;
+SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET default_tablespace = '';
