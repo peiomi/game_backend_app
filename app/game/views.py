4681b81f143767efd76407ea38c67e5947dec3f9
paydon <paydonriley@gmail.com>
2026-02-09 17:18:48 -0600

    event views

--- app/game/views.py
+++ app/game/views.py
@@ -153,6 +153,40 @@ def session_detail(request, pk):
     elif request.method == 'DELETE':
         session.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# event views
+@api_view(['GET', 'POST'])
+def event_list(request):
+    if request.method == 'GET':
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    if request.method == 'GET':
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_vaild():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 
 # def index(request):
