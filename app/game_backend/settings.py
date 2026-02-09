c9c39481eb3bc7749d44df5372e8749addf32f37
paydon <paydonriley@gmail.com>
2026-02-09 11:59:41 -0600

    used Django's AbstractUser for Player model

--- app/game_backend/settings.py
+++ app/game_backend/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# User model
+
+AUTH_USER_MODEL = 'players.Player'
--- app/players/models.py
+++ app/players/models.py
@@ -1,12 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-
-class Player(models.Model):
-    username = models.CharField(max_length=70, blank=False, default='')
-    password = models.CharField(max_length=70, blank=False, default='')
-    first_name = models.CharField(max_length=70, blank=False, default='')
-    last_name = models.CharField(max_length=70, blank=False, default='')
-    email = models.CharField(max_length=70, blank=False, default='')
-
-
+class Player(AbstractUser):
+    account_created = models.DateTimeField(auto_now_add=True)
+    last_login_ip = models.GenericIPAddressField(null=True, blank=True, default='')
