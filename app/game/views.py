33272cc5e4e918be0463bbe3c664d36bb7da9fce
paydon <paydonriley@gmail.com>
2026-02-09 17:26:22 -0600

    quest views

--- app/game/views.py
+++ app/game/views.py
@@ -221,6 +221,39 @@ def item_detail(request, pk):
         item.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+# quest views
+@api_view(['GET', 'POST'])
+def quest_list(request):
+    if request.method == 'GET':
+        quests = Quest.objects.all()
+        serializer = QuestSerializer(quests, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = QuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def quest_detail(request, pk):
+    quest = get_object_or_404(Quest, pk=pk)
+    if request.method == 'GET':
+        serializer = QuestSerializer(quest)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = QuestSerializer(quest, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        quest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 
 # def index(request):
