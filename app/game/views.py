38d8561b90529022699a9661e13b4a928c0d0aec
paydon <paydonriley@gmail.com>
2026-02-09 16:45:07 -0600

    pc views

--- app/game/views.py
+++ app/game/views.py
@@ -55,6 +55,38 @@ def spell_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+# pc views
+@api_view(['GET', 'POST'])
+def pc_list(request):
+    if request.method == 'GET':
+        all_playable_characters = PC.objects.all()
+        serializer = PCSerializer(all_playable_characters, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = PCSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def pc_detail(request, pk):
+    pc = get_object_or_404(PC, pk=pk)
+    if request.method == 'GET':
+        serializer = PCSerializer(pc)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = PCSerializer(pc, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        pc.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
@@ -69,72 +101,3 @@ def spell_detail(request, pk):
         queryset = Tutorial.objects.all()
         return Response({'tutorials': queryset}) """
 
-
-
-@api_view(['GET', 'POST', 'DELETE'])
-def inventory_list(request):
-    if request.method == 'GET':
-        inventory = Inventory.objects.all()
-
-        player_c = request.GET.get('player_c', None)
-        if player_c is not None:
-            inventory = inventory.filter(player_c__icontains=player_c)
-
-        inventory_serializer = InventorySerializer(inventory, many=True)
-        return JsonResponse(inventory_serializer.data, safe=False)
-        # 'safe=False' for objects serialization
-
-    elif request.method == 'POST':
-        inventory_data = JSONParser().parse(request)
-        inventory_serializer = InventorySerializer(data=inventory_data)
-        if inventory_serializer.is_valid():
-            inventory_serializer.save()
-            return JsonResponse(inventory_serializer.data,
-                                status=status.HTTP_201_CREATED)
-        return JsonResponse(inventory_serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        count = Inventory.objects.all().delete()
-        return JsonResponse(
-            {
-                'message':
-                '{} Inventory were deleted successfully!'.format(count[0])
-            },
-            status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def tutorial_detail(request, pk):
-    try:
-        tutorial = Tutorial.objects.get(pk=pk)
-    except Tutorial.DoesNotExist:
-        return JsonResponse({'message': 'The tutorial does not exist'},
-                            status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        tutorial_serializer = TutorialSerializer(tutorial)
-        return JsonResponse(tutorial_serializer.data)
-
-    elif request.method == 'PUT':
-        tutorial_data = JSONParser().parse(request)
-        tutorial_serializer = TutorialSerializer(tutorial, data=tutorial_data)
-        if tutorial_serializer.is_valid():
-            tutorial_serializer.save()
-            return JsonResponse(tutorial_serializer.data)
-        return JsonResponse(tutorial_serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        tutorial.delete()
-        return JsonResponse({'message': 'Tutorial was deleted successfully!'},
-                            status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET'])
-def tutorial_list_published(request):
-    tutorials = Tutorial.objects.filter(published=True)
-
-    if request.method == 'GET':
-        tutorials_serializer = TutorialSerializer(tutorials, many=True)
-        return JsonResponse(tutorials_serializer.data, safe=False)
